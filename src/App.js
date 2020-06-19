9f346a9b81ec85d34900595ef5f775c8e8e52c2a
Allison Usher <allyjay317@gmail.com>
2020-06-18 19:50:45 -0700

    reverse commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import User from './Components/User';
 import Recipe from './Components/Recipe';
+import Signup from './Components/Signup';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/'>
-        <Home />
-      </Route>
-      <Route exact path='/:userid'>
-        <User />
-      </Route>
-      <Route path='/:userid/:recipeid'>
-        <Recipe />
-      </Route>
+      <Switch>
+        <Route path='/signup'>
+          <Signup />
+        </Route>
+        <Route path='/:userid/:recipeid'>
+          <Recipe />
+        </Route>
+        <Route path='/:userid'>
+          <User />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
