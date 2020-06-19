45fac5f1d68f03cacb4b29fb17546daa37bd0dea
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-06-19 00:22:31 -0400

    fixed missing semicolon

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Router>
